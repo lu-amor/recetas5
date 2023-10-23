11f110bf6e940b9202c74d8b01cf185aa89bdd6e
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2023-10-23 10:00:13 -0300

    ej terminado

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(IPrinterText printerText)
         {
-            Console.WriteLine(recipe.GetTextToPrint());
+            Console.WriteLine(printerText.GetTextToPrint());
         }
     }
 }
